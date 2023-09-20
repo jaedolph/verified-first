8615d5076a079948cf13065888d52abfa5c89586
Jack Adolph <jack.adolph@gmail.com>
2023-09-20 20:54:56 +1000

    Add console scripts

--- ebs/verifiedfirst/__main__.py
+++ ebs/verifiedfirst/__main__.py
@@ -1,6 +1,12 @@
 """Main entrypoint."""
 from verifiedfirst import create_app
 
-if __name__ == "__main__":
+
+def main() -> None:
+    """Run the verifiedfirst web server."""
     app = create_app()
     app.run(host="0.0.0.0")
+
+
+if __name__ == "__main__":
+    main()

437052cbb6390a830e7f47133074892e812a0c10
Jack Adolph <jack@jaedolph.net>
2023-10-18 20:13:55 +1100

    Add EBS endpoint to check if broadcaster's auth token is valid

--- ebs/tests/test_auth.py
+++ ebs/tests/test_auth.py
@@ -67,3 +67,83 @@ def test_auth_error(app, client, mocker):  # pylint: disable=unused-argument
     assert resp.status_code == 200
     assert resp.text == bad_response
     mock_get_auth_tokens.assert_called_with(defaults.AUTH_CODE)
+
+
+def test_auth_check(app, client, mocker):  # pylint: disable=unused-argument
+    """Test /auth/check endpoint works correctly."""
+
+    mock_jwt = mocker.patch("verifiedfirst.verify.verify_jwt")
+    mock_jwt.return_value = (defaults.CHANNEL_ID, "broadcaster")
+
+    mock_get_broadcaster = mocker.patch("verifiedfirst.twitch.get_broadcaster")
+    mock_broadcaster = mocker.Mock()
+    mock_broadcaster.access_token = defaults.AUTH_ACCESS_TOKEN
+    mock_get_broadcaster.return_value = mock_broadcaster
+    mock_get_broadcaster_from_token = mocker.patch(
+        "verifiedfirst.twitch.get_broadcaster_from_token"
+    )
+
+    resp = client.get(url_for("auth.auth_check"))
+
+    assert resp.json["auth_status"] == "OK"
+    assert resp.status_code == 200
+    mock_get_broadcaster_from_token.assert_called_with(defaults.AUTH_ACCESS_TOKEN)
+
+
+def test_auth_check_not_broadcaster(app, client, mocker):  # pylint: disable=unused-argument
+    """Test /auth/check fails if the user accessing it is not a broadcaster."""
+
+    mock_jwt = mocker.patch("verifiedfirst.verify.verify_jwt")
+    mock_jwt.return_value = (defaults.CHANNEL_ID, "viewer")
+    mock_get_broadcaster = mocker.patch("verifiedfirst.twitch.get_broadcaster")
+    mock_get_broadcaster_from_token = mocker.patch(
+        "verifiedfirst.twitch.get_broadcaster_from_token"
+    )
+    resp = client.get(url_for("auth.auth_check"))
+
+    assert resp.status_code == 403
+    assert resp.json["error"] == "user role is not broadcaster"
+    mock_get_broadcaster.assert_not_called()
+    mock_get_broadcaster_from_token.assert_not_called()
+
+
+def test_auth_check_broadcaster_not_found(app, client, mocker):  # pylint: disable=unused-argument
+    """Test /auth/check fails if the broadcaster is not in the database."""
+
+    mock_jwt = mocker.patch("verifiedfirst.verify.verify_jwt")
+    mock_jwt.return_value = (defaults.CHANNEL_ID, "broadcaster")
+    mock_get_broadcaster = mocker.patch("verifiedfirst.twitch.get_broadcaster")
+    mock_get_broadcaster.return_value = None
+    mock_get_broadcaster_from_token = mocker.patch(
+        "verifiedfirst.twitch.get_broadcaster_from_token"
+    )
+
+    resp = client.get(url_for("auth.auth_check"))
+
+    assert resp.status_code == 403
+    assert resp.json["error"] == "broadcaster is not authed yet"
+    mock_get_broadcaster.assert_called_with(defaults.CHANNEL_ID)
+    mock_get_broadcaster_from_token.assert_not_called()
+
+
+def test_auth_check_auth_invalid(app, client, mocker):  # pylint: disable=unused-argument
+    """Test /auth/check fails if the auth token in the database is invalid/stale."""
+
+    mock_jwt = mocker.patch("verifiedfirst.verify.verify_jwt")
+    mock_jwt.return_value = (defaults.CHANNEL_ID, "broadcaster")
+
+    mock_get_broadcaster = mocker.patch("verifiedfirst.twitch.get_broadcaster")
+    mock_broadcaster = mocker.Mock()
+    mock_broadcaster.access_token = defaults.AUTH_ACCESS_TOKEN
+    mock_get_broadcaster.return_value = mock_broadcaster
+    mock_get_broadcaster_from_token = mocker.patch(
+        "verifiedfirst.twitch.get_broadcaster_from_token"
+    )
+    mock_get_broadcaster_from_token.side_effect = HTTPError
+
+    resp = client.get(url_for("auth.auth_check"))
+
+    assert resp.status_code == 403
+    assert resp.json["error"] == "broadcaster auth is invalid"
+    mock_get_broadcaster.assert_called_with(defaults.CHANNEL_ID)
+    mock_get_broadcaster_from_token.assert_called_with(defaults.AUTH_ACCESS_TOKEN)
--- ebs/verifiedfirst/auth/routes.py
+++ ebs/verifiedfirst/auth/routes.py
@@ -1,7 +1,17 @@
 """Auth routes."""
-from flask import Blueprint, Response, make_response, render_template, request, current_app
+from flask import (
+    Blueprint,
+    Response,
+    make_response,
+    render_template,
+    abort,
+    jsonify,
+    request,
+    current_app,
+)
+from requests import RequestException
 
-from verifiedfirst import twitch
+from verifiedfirst import twitch, verify
 
 bp = Blueprint("auth", __name__)
 
@@ -28,3 +38,26 @@ def auth() -> Response:
         auth_msg = "AUTH_FAILED"
 
     return make_response(render_template("auth.html", auth_msg=auth_msg))
+
+
+@bp.route("/auth/check", methods=["GET"])
+@verify.token_required
+def auth_check(channel_id: int, role: str) -> Response:
+    """Checks if the broadcaster auth is valid."""
+
+    # pylint: disable=duplicate-code
+    if role != "broadcaster":
+        abort(403, "user role is not broadcaster")
+
+    # check broadcaster exists in database
+    broadcaster = twitch.get_broadcaster(channel_id)
+    if broadcaster is None:
+        abort(403, "broadcaster is not authed yet")
+    # pylint: disable=
+
+    try:
+        twitch.get_broadcaster_from_token(broadcaster.access_token)
+    except RequestException:
+        abort(403, "broadcaster auth is invalid")
+
+    return make_response(jsonify({"auth_status": "OK"}))

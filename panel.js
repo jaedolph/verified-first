f060c7cd2540ea83029ab12a652f03cf3f0b55da
jaedolph <10444633+jaedolph@users.noreply.github.com>
2024-10-02 21:08:42 +1000

    Add 'Custom' date range picker

--- panel.js
+++ panel.js
@@ -10,12 +10,19 @@ let title = defaultTitle
 const monthButton = document.getElementById('month')
 const yearButton = document.getElementById('year')
 const allTimeButton = document.getElementById('all_time')
+const customRangeButton = document.getElementById('custom_range')
+const datePicker = document.getElementById('date_picker')
 const firstsText = document.getElementById('firsts')
 const lastUpdatedText = document.getElementById('lastupdated')
+const startDateInput = document.getElementById('start_date_input')
+const endDateInput = document.getElementById('end_date_input')
 
 monthButton.addEventListener('click', getFirstsMonth)
 yearButton.addEventListener('click', getFirstsYear)
 allTimeButton.addEventListener('click', getFirstsAllTime)
+customRangeButton.addEventListener('click', showDateRangePicker)
+startDateInput.addEventListener('change', getFirstsCustom)
+endDateInput.addEventListener('change', getFirstsCustom)
 
 // Get "fists" counts after the user is authorized
 twitch.onAuthorized(function (auth) {
@@ -58,10 +65,14 @@ twitch.configuration.onChanged(function () {
 * Get first counts from the EBS and update the display
 * @param {String} authorization - authorization header to send to the EBS
 * @param {Date} startTime - get all firsts from this date
+* @param {Date} endTime - get all firsts to this date
 */
-async function getFirsts (authorization, startTime) {
+async function getFirsts (authorization, startTime, endTime) {
   let firstsUrl = extensionUri + '/firsts'
-  if (startTime) {
+  if (startTime && endTime) {
+    firstsUrl = firstsUrl + '?' + new URLSearchParams({ start_time: startTime.toISOString(), end_time: endTime.toISOString() })
+  }
+  if (startTime && !endTime) {
     firstsUrl = firstsUrl + '?' + new URLSearchParams({ start_time: startTime.toISOString() })
   }
 
@@ -138,45 +149,107 @@ function groupFirsts (firsts) {
 }
 
 /**
+* Show the custom date range picker
+*/
+function showDateRangePicker () {
+  // set default custom range to the previous month
+  const currentTime = new Date()
+  const lastMonthEnd = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), 1))
+  const lastMonthStart = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth() - 1, 1))
+
+  // set the default input values
+  datePicker.className = ''
+  firstsText.innerHTML = ''
+  startDateInput.value = formatDate(lastMonthStart)
+  endDateInput.value = formatDate(lastMonthEnd)
+
+  // get firsts using the default input values
+  getFirstsCustom()
+
+  // highlight the "Custom" button
+  monthButton.innerHTML = 'Month'
+  yearButton.innerHTML = 'Year'
+  allTimeButton.innerHTML = 'All Time'
+  customRangeButton.innerHTML = '<strong>Custom</strong>'
+}
+
+/**
+* Hide the custom date range picker
+*/
+function hideDateRangePicker () {
+  datePicker.className = 'hidden'
+}
+
+/**
+* Convert a date to a format compatible with an html date input
+*/
+function formatDate (date) {
+  const dateString = date.toISOString().split('T')[0]
+  return dateString
+}
+
+/**
 * Get first counts for current month
 */
 function getFirstsMonth () {
+  // hide the date picker if required
+  hideDateRangePicker()
+
   // set start time to the first day of the current month
   const currentTime = new Date()
   const startTime = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), 1))
 
-  getFirsts(authorization, startTime)
+  getFirsts(authorization, startTime, null)
 
   // highlight the "Month" button
   monthButton.innerHTML = '<strong>Month</strong>'
   yearButton.innerHTML = 'Year'
   allTimeButton.innerHTML = 'All Time'
+  customRangeButton.innerHTML = 'Custom'
 }
 
 /**
 * Get first counts for current year
 */
 function getFirstsYear () {
+  // hide the date picker if required
+  hideDateRangePicker()
+
   // set start time to the first day of the current year
   const currentTime = new Date()
   const startTime = new Date(Date.UTC(currentTime.getUTCFullYear(), 0, 1))
 
-  getFirsts(authorization, startTime)
+  getFirsts(authorization, startTime, null)
 
   // highlight the "Year" button
   monthButton.innerHTML = 'Month'
   yearButton.innerHTML = '<strong>Year</strong>'
   allTimeButton.innerHTML = 'All Time'
+  customRangeButton.innerHTML = 'Custom'
 }
 
 /**
 * Get first counts for all time
 */
 function getFirstsAllTime () {
-  getFirsts(authorization, null)
+  // hide the date picker if required
+  hideDateRangePicker()
+
+  getFirsts(authorization, null, null)
 
   // highlight the "All Time" button
   monthButton.innerHTML = 'Month'
   yearButton.innerHTML = 'Year'
   allTimeButton.innerHTML = '<strong>All Time</strong>'
+  customRangeButton.innerHTML = 'Custom'
+}
+
+/**
+* Get first counts for a custom range
+*/
+function getFirstsCustom () {
+  const startDateInput = document.getElementById('start_date_input')
+  const endDateInput = document.getElementById('end_date_input')
+
+  getFirsts(authorization, startDateInput.valueAsDate, endDateInput.valueAsDate)
 }
